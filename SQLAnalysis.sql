177c284095cf9df5baf59ad1fa583c3ea2e909de
Adhil Muhamed <mohammedadhil17@gmail.com>
2023-09-25 00:01:03 +0530

    c5

--- SQLAnalysis.sql
+++ SQLAnalysis.sql
@@ -2,28 +2,23 @@ create database CreditApproval;
 
 USE CreditApproval;
 
-CREATE TABLE final_data (
-    Gender INT,
-    Car_Owner INT,
-    Propert_Owner INT,
-    Children INT,
-    Annual_income INT,
-    Type_Income INT,
-    Education INT,
-    Marital_status INT,
-    Housing_type INT,
-    Age INT,
-    Employed_exp INT,
-    Type_Occupation INT,
-    Family_Members INT,
-    label INT
-);
+select  * from final_data;
 
-LOAD DATA local INFILE 'C:\Users\moham\Documents\CreditCardApproval_Prediction\final_project'
-INTO TABLE final_data
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
+# 1. Group the customers based on their income type and find the average of their annual income.
 
-mysql -u your_username -p your_database < path/to/yourfile.csv
+select Type_Income, round(avg(annual_income),2) As Average_Income from final_data group by Type_Income;
+
+# 2. Find the female owners of cars and property.
+
+select * from final_data where Gender = 'F' AND Car_Owner  = 'Y' AND Propert_Owner = 'Y' ;
+
+# 3. Find the male customers who are staying with their families.
+
+select * from final_data where Gender = 'M' AND Housing_type  = 'With parents';
+
+# 4. Please list the top five people having the highest income.
+
+select * from final_data order by Annual_income DESC Limit 5;
+
+
 
